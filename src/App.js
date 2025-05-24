b883bb73b3d91c034d7d6a966a7143aa97c5e624
Ali-gunner <ali.ranjbar.lut2024@gmail.com>
2025-05-24 12:23:03 +0300

    AddFriendForm is added

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add friend</Button>
       </div>
     </div>
   );
@@ -58,7 +60,25 @@ function Friend({ friend }) {
       )}
       {friend.balance === 0 && <p>You your {friend.name} are even</p>}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👩🏼‍🤝‍👩🏻 Friend name</label>
+      <input type="text" />
+
+      <label>Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
   );
 }
