621c4051c51257300686b6e6b8dbb3be010798cd
Ali-gunner <ali.ranjbar.lut2024@gmail.com>
2025-05-24 12:43:43 +0300

    Splitting bill form is added

--- src/App.js
+++ src/App.js
@@ -27,6 +27,8 @@ export default function App() {
         <FormAddFriend />
         <Button>Add friend</Button>
       </div>
+
+      <FormSplitBill />
     </div>
   );
 }
@@ -77,6 +79,29 @@ function FormAddFriend() {
 
       <label>Image URL</label>
       <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>Bill value</label>
+      <input type="text" />
+      <label>Your expense</label>
+      <input type="text" />
+      <label>X's expense</label>
+      <input type="text" disabled />
+
+      <label>Who is paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
 
       <Button>Add</Button>
     </form>
